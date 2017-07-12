fa1ef2c1b866c0ecc13a1398bd0c7b4cadafcb1a
j-morgan93 <jonathandm93@gmail.com>
2017-07-11 17:30:47 -0700

    Temp, and (Temp,rho1) mod filles
    just playing aound with optimization models. They require iterpolation

--- scip_spec.py
+++ scip_spec.py
@@ -4,30 +4,51 @@ Created on Mon Jul 10 09:51:00 2017
 
 @author: jmorga14
 """
-
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import minimize
+from scipy.interpolate import interp1d
 
 sfile = "C:/Users/jmorga14/AnacondaProjects/matplot_gui-reading/simulated.out"
 efile = "C:/Users/jmorga14/AnacondaProjects/matplot_gui-reading/experimental.out"
 simfile =np.genfromtxt(sfile, comments='#', skip_header=3, names=True, unpack=True, autostrip=True)
-expfile = np.genfromtxt(efile,comments='(',skip_header=1,names=True,unpack=True,autostrip=True, dtype=float)
-   
+expfile = np.genfromtxt(efile, comments='#', skip_header=3, names=True, unpack=True, autostrip=True)
+# should replace "names" with the temperature values
 sf = np.zeros(shape=(len(simfile),len(simfile.dtype.names)),dtype=float)
-# ef = np.zeros(shape=(len(simfile),len(simfile.dtype.names)),dtype=float))
-ef = expfile[expfile.dtype.names[1]]
+ef = expfile[expfile.dtype.names[1]] / max(expfile[expfile.dtype.names[1]])
+names = list(simfile.dtype.names)
+maxsim = np.zeros(shape=(len(simfile.dtype.names),1),dtype=float)
 
-for i in range(len(simfile.dtype.names)):
-    sf[:,i] = simfile[simfile.dtype.names[i]]
+for i in range(1,len(simfile.dtype.names[1:])+1):
+    maxsim[i] = max(simfile[simfile.dtype.names[i]])
+    sf[:,i] = simfile[simfile.dtype.names[i]] / maxsim[i]
+    names[i] = str(i)
+    
+def obj(i):  
+    d = sum(abs(ef-sf[:,i])*pow(ef, 3))
+    return d
 
-def obj(i):
-    print("PICKLED IS ",i)
+deltas = np.zeros(shape=(len(simfile.dtype.names[1:]),1),dtype=float)
 
-    i = int(i+i/2)
-    print("CHANGED IS ",i)
+for i in range(len(deltas)):
+    deltas[i] =obj(i+1)
 
-    pf= sf[:,i]
-    return sum(abs(ef-pf)*pow(ef,2))
+deltas = np.asarray(deltas).squeeze()
+xp  = np.linspace(1,len(simfile.dtype.names[1:]),num=len(simfile.dtype.names[1:]))
+real_obj = interp1d(xp, deltas, kind='cubic')
 
-x0=10
-sol = minimize(obj,x0,method='Nelder-Mead',options={'disp': True,'maxiter':100}, tol=1e-3)
+def realguy(i):
+    print(i)
+    print(real_obj(i))
+    print(2700+i*10)
+    return real_obj(i)
+
+x0=4
+
+sol = minimize(realguy,x0,method='Nelder-Mead',options={'disp': True, 'maxiter':100}, tol=1e-6)
+
+xnew = np.linspace(1,19,num=100)
+
+plt.plot(xp,deltas,'o',xnew,real_obj(xnew),'--')
+plt.legend(['tabulated','interpolated'],loc='best')
+plt.show()
---
+++ scip_temp_nrho.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 11 14:50:09 2017
+
+@author: jmorga14
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 10 09:51:00 2017
+
+@author: jmorga14
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import minimize
+from scipy.interpolate import interp2d
+from scipy.interpolate import Rbf
+from scipy.interpolate import griddata
+
+sfile = "C:/Users/jmorga14/AnacondaProjects/matplot_gui-reading/temp_nrho.out"
+efile = "C:/Users/jmorga14/AnacondaProjects/matplot_gui-reading/exp_temp_nrho.out"
+simfile =np.genfromtxt(sfile, comments='#', skip_header=3, names=True, unpack=True, autostrip=True)
+expfile = np.genfromtxt(efile, comments='#', skip_header=3, names=True, unpack=True, autostrip=True)
+# should replace "names" with the temperature values
+sf = np.zeros(shape=(int(len(simfile.dtype.names[1:]) / 5), 5, len(simfile),), dtype=float)
+ef = expfile[expfile.dtype.names[1]] / max(expfile[expfile.dtype.names[1]])
+names = list(simfile.dtype.names)
+maxsim = np.zeros(shape=(int(len(simfile.dtype.names[1:]) / 5), 5), dtype=float)
+count = 0
+for i in range(int(len(simfile.dtype.names[1:]) / 5)):
+    for j in range(5):
+        maxsim[i, j] = max(simfile[simfile.dtype.names[i*5+j]])
+        sf[i, j, :] = simfile[simfile.dtype.names[i*5+j]] / maxsim[i, j]
+        count += 1
+    
+def obj(i, j):  
+    d = sum(abs(ef-sf[i, j, :])*pow(ef, 2))
+    return d
+
+deltas = np.zeros(shape=(20, 5), dtype=float)
+points = np.zeros(shape=(100, 2), dtype=float)
+nrho = np.linspace(1, 5, 5)
+temp = np.linspace(1, 20, 20)
+count = 0
+for i in range(int(len(simfile.dtype.names[1:]) / 5)):
+    for j in range(5):
+        for k in range(2):
+            if k == 0:
+                points[count, k] = temp[i]
+            if k == 1:
+                points[count, k] = nrho[j]
+            deltas[i,j] = obj(i, j)
+        count += 1
+
+grid_x, grid_y = np.mgrid[1:5:.05, 2400:3350:1]
+
+deltas = np.asarray(deltas).squeeze()
+
+data_interp = interp2d(temp , nrho, deltas.T, kind='linear')
+
+# data_interp = griddata( points, deltas, (grid_y, grid_x), method='cubic')
+"""
+deltas = np.asarray(deltas).squeeze()
+xp  = np.linspace(1,len(simfile.dtype.names[1:]),num=len(simfile.dtype.names[1:]))
+real_obj = interp1d(xp, deltas, kind='cubic')
+
+def realguy(i):
+    print(i)
+    print(real_obj(i))
+    print(2700+i*10)
+    return real_obj(i)
+
+x0=4
+"""
+def realguy(x):
+    i = x[0]
+    j = x[1]
+    print("I :",i)
+    print(" J:",j)
+    print(data_interp(i,j))
+    return data_interp(i, j)
+
+x0 = [6,4]
+
+sol = minimize(realguy,x0,method='Nelder-Mead',options={'disp': True, 'maxiter':100}, tol=1e-6)
+
+"""
+xnew = np.linspace(1,19,num=100)
+
+plt.plot(xp,deltas,'o',xnew,real_obj(xnew),'--')
+plt.legend(['tabulated','interpolated'],loc='best')
+plt.show()"""
